17f25a53db61e10dd02716b75183bbe5dd023fe1
Gabriel dos Santos <developergabrieldossantos@gmail.com>
2025-09-15 17:08:54 -0300

    List of the categories

--- src/StupidDatabase.java
+++ src/StupidDatabase.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 public class StupidDatabase {
     public static void main(String[] args) {
-        Category javaCategory = new Category("java");
-        Category pythonCategory = new Category("python");
 
+        // Lista de categorias
+        ArrayList<Category> categories = new ArrayList<>();
+
+        // Categorias
+        Category javaCategory = new Category("java");
+        categories.add(javaCategory);
+
+        Category pythonCategory = new Category("python");
+        categories.add(pythonCategory);
+
+        // Materias instanciados
         Material materialOne = new Material("Java tutorial", "Uma aula de java para iniciantes", "Gabriel dos Santos", "#", javaCategory.getName()
         , 0);
         javaCategory.addMaterial(materialOne);
